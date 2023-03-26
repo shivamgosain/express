d2cdd26c6947381e81b49e245fd23571014340de
shivamgosain <gosain.shivam89@gmail.com>
2023-03-26 23:42:02 +0530

    test

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello World', "Akki");
+  res.send('Hello World, Akki');
 });
 
 /* istanbul ignore next */
